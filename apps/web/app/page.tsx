d5b124c84c134212302cd39734519de6a9a04b86
Tabish1511 <khaqantabish@gmail.com>
2024-07-12 00:33:21 +0200

    hacked a basic chat app, need to build from here, create separate components for features

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -26,11 +26,22 @@ const WebSocketComponent = () => {
     </div>
   }
 
-  return <div>
-      <ChatComponent/>   
-      </div>
-  
-  
+  return <div className='h-screen flex justify-center'>
+      <div className='w-2/6 flex flex-col justify-center'>
+        <ChatComponent messagesObject={allMessages} />
+        <div className='flex justify-center'>
+          <input className='rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4'
+          onChange={(e) => {
+          setMessage(e.target.value);
+          }} value={message} placeholder='Type your message'></input>
+          
+          <button className='rounded-lg bg-indigo-800 border-sky-500 w-32 py-4'
+          onClick={() => {
+            socket.send(message);
+          }}>Send</button>
+        </div>
+      </div>   
+    </div>
 }
 
 WebSocketComponent.displayName = 'WebSocketComponent';
--- packages/ui/src/chatComponent.tsx
+++ packages/ui/src/chatComponent.tsx
@@ -1,5 +1,97 @@
-export function ChatComponent() {
-    return <div className="border-solid border-2 border-sky-500">
-        Chat component
+import { MessageComponent } from "@repo/ui/messageComponent";
+
+
+interface ChatComponentProps {
+    messagesObject: string[];
+}
+
+export function ChatComponent(props: ChatComponentProps) {
+    const messagesArray = Object.entries(props.messagesObject);
+    
+    return <div className="bg-gray-800 rounded-lg w-full h-lvh my-20 flex flex-col justify-center">
+            {Object.entries(props.messagesObject).map(([key, value]) => (
+            <div key={key}>
+                <MessageComponent message={value} /> <br/>
+            </div>
+            ))}
     </div>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const [allMessages, setAllMessages] = useState<string[]>([]);
+
+//     useEffect(() => {
+//         setAllMessages(prevMessages => [...prevMessages, props.allMessages])
+        
+//       }, [])
+
+//     if(!allMessages){
+//         return <div>
+//             No messages yet...
+//         </div>
+//     }
+
+//     return <div className="bg-gray-800 rounded-lg w-full h-lvh my-20 flex flex-col justify-end">
+//         <div className="border-solid border-2 border-sky-500 w-full h-max flex justify-center">
+//             {props.messagesObject}
+
+//             {Object.entries(props.allMessages).map(([key, value]) => (
+//             <div key={key}>
+//                 <MessageComponent message={typeof(value)} /> <br/>
+//             </div>
+//             ))}
+//         </div>
+//     </div>
