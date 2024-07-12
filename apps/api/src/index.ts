e85f0c5f455ada0aabd4b3e48aec90d546c3440b
Tabish1511 <khaqantabish@gmail.com>
2024-07-12 23:19:17 +0200

    deploy basic chat app - try success

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -14,51 +14,9 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function message(data, isBinary) {
 
     wss.clients.forEach(function each(client) {
-      // if (client.readyState === WebSocket.OPEN) {
       if (client.readyState === 1) {
         client.send(data, { binary: isBinary });
       }
     });
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express";
-
-// const app = express()
-
-// app.get("/", (req, res) => {
-//     res.json({
-//         message: "hello world"
-//     });
-// })
-
-// app.listen(8080, () => {
-//     console.log("Listening on port 8080");
-// })
