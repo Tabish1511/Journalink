eeba0e96786429b9784d1450158c4abcc277c225
Tabish1511 <khaqantabish@gmail.com>
2024-07-12 23:05:06 +0200

    local - /api testing for deploy

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -14,7 +14,8 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function message(data, isBinary) {
 
     wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+      // if (client.readyState === WebSocket.OPEN) {
+      if (client.readyState === 1) {
         client.send(data, { binary: isBinary });
       }
     });
