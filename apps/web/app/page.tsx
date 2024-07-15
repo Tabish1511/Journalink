bd04766c1f4610d5ff347b93225629e83cdefa29
Tabish1511 <khaqantabish@gmail.com>
2024-07-15 21:38:24 +0200

    main - .env => prod

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,10 +6,9 @@ const WebSocketComponent = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const [allMessages, setAllMessages] = useState<string[]>([]);
   const [message, setMessage] = useState<string>("");
-  const baseEndpoint = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+  const baseEndpoint = process.env.NEXT_PUBLIC_API_URL as string;
 
   useEffect(() => {
-    // const newSocket = new WebSocket(process.env.NODE_ENV === 'production' ? baseEndpoint : 'http://localhost:8080');
     const newSocket = new WebSocket(baseEndpoint);
     newSocket.onopen = () => {
       console.log('Connection established');
