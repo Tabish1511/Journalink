d73e909a5451452186b9fbcd09dc9feb84094042
Tabish1511 <khaqantabish@gmail.com>
2024-07-17 13:33:21 +0200

    main - moving WS to chatcomponent 1

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,52 +1,9 @@
-"use client"
-import { useEffect, useState } from 'react'
-import {ChatComponent} from '@repo/ui/chatComponent'
-import {AllMessagesComponent} from '@repo/ui/allMessagesComponent'
+import ChatComponent from '@repo/ui/chatComponent'
 
-const WebSocketComponent = () => {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [allMessages, setAllMessages] = useState<string[]>([]);
-  const [message, setMessage] = useState<string>("");
-  const baseEndpoint = process.env.NEXT_PUBLIC_API_URL as string;
-
-  useEffect(() => {
-    const newSocket = new WebSocket(baseEndpoint);
-    newSocket.onopen = () => {
-      console.log('Connection established');
-      setSocket(newSocket);
-    }
-    newSocket.onmessage = (message) => {
-      console.log('Message received:', message);
-      setAllMessages(prevMessages => [...prevMessages, message.data])
-    }
-    
-  }, [])
-
-  if(!socket){
-    return <div>
-      Loading messages...
+export default function Home() {
+  return (
+    <div>
+      <ChatComponent />
     </div>
-  }
-
-  return <div className='h-screen flex justify-center'>
-      <div className='w-2/6 flex flex-col justify-center'>
-        {/* <ChatComponent messagesObject={allMessages} /> */}
-        <AllMessagesComponent messagesObject={allMessages} />
-        <div className='flex justify-center'>
-          <input className='rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4'
-          onChange={(e) => {
-          setMessage(e.target.value);
-          }} value={message} placeholder='Type your message'></input>
-          
-          <button className='rounded-lg bg-indigo-800 border-sky-500 w-32 py-4'
-          onClick={() => {
-            socket.send(message);
-          }}>Send</button>
-        </div>
-      </div>   
-    </div>
+  )
 }
-
-WebSocketComponent.displayName = 'WebSocketComponent';
-
-export default WebSocketComponent;
--- packages/ui/src/chatComponent.tsx
+++ packages/ui/src/chatComponent.tsx
@@ -1,97 +1,46 @@
-import { MessageComponent } from "@repo/ui/messageComponent";
+"use client"
+import { useEffect, useState } from 'react'
+import {AllMessagesComponent} from '@repo/ui/allMessagesComponent'
 
+export default function ChatComponent() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [allMessages, setAllMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const baseEndpoint = process.env.NEXT_PUBLIC_API_URL as string;
 
-interface ChatComponentProps {
-    messagesObject: string[];
-}
-
-export function ChatComponent(props: ChatComponentProps) {
-    const messagesArray = Object.entries(props.messagesObject);
+  useEffect(() => {
+    const newSocket = new WebSocket(baseEndpoint);
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      setSocket(newSocket);
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message);
+      setAllMessages(prevMessages => [...prevMessages, message.data])
+    }
     
-    return <div className="bg-gray-800 rounded-lg w-full h-lvh my-20 flex flex-col justify-center">
-            {Object.entries(props.messagesObject).map(([key, value]) => (
-            <div key={key}>
-                <MessageComponent message={value} /> <br/>
-            </div>
-            ))}
+  }, [])
+
+  if(!socket){
+    return <div>
+      Loading messages...
+    </div>
+  }
+
+  return <div className='h-screen flex justify-center'>
+      <div className='w-2/6 flex flex-col justify-center'>
+        <AllMessagesComponent messagesObject={allMessages} />
+        <div className='flex justify-center'>
+          <input className='rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4'
+          onChange={(e) => {
+          setMessage(e.target.value);
+          }} value={message} placeholder='Type your message'></input>
+          
+          <button className='rounded-lg bg-indigo-800 border-sky-500 w-32 py-4'
+          onClick={() => {
+            socket.send(message);
+          }}>Send</button>
+        </div>
+      </div>   
     </div>
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const [allMessages, setAllMessages] = useState<string[]>([]);
-
-//     useEffect(() => {
-//         setAllMessages(prevMessages => [...prevMessages, props.allMessages])
-        
-//       }, [])
-
-//     if(!allMessages){
-//         return <div>
-//             No messages yet...
-//         </div>
-//     }
-
-//     return <div className="bg-gray-800 rounded-lg w-full h-lvh my-20 flex flex-col justify-end">
-//         <div className="border-solid border-2 border-sky-500 w-full h-max flex justify-center">
-//             {props.messagesObject}
-
-//             {Object.entries(props.allMessages).map(([key, value]) => (
-//             <div key={key}>
-//                 <MessageComponent message={typeof(value)} /> <br/>
-//             </div>
-//             ))}
-//         </div>
-//     </div>
