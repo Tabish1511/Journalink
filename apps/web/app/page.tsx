8fb44911633eab0521e69b0f7fb3c44b152994b7
Tabish1511 <khaqantabish@gmail.com>
2024-07-17 12:51:50 +0200

    main - try w/o chatcomp

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -30,7 +30,7 @@ const WebSocketComponent = () => {
 
   return <div className='h-screen flex justify-center'>
       <div className='w-2/6 flex flex-col justify-center'>
-        <ChatComponent messagesObject={allMessages} />
+        {/* <ChatComponent messagesObject={allMessages} /> */}
         <AllMessagesComponent messagesObject={allMessages} />
         <div className='flex justify-center'>
           <input className='rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4'
