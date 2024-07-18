dea30f965ecf40c6e06e7762d638e2a0db20449e
Tabish1511 <khaqantabish@gmail.com>
2024-07-18 17:07:54 +0200

    nextauth - reroute from landing page to signin if no session

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { useSession, signIn } from "next-auth/react";
+import { useEffect } from "react";
 import Appbar from "@repo/ui/appBar";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (!session && status !== "loading") {
+      signIn();
+    }
+  }, [session, status]);
+
+  if(status === "loading"){
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <Appbar />
     </div>
-  )
+  );
 }
