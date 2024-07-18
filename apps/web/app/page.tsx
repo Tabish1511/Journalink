7480051f5a7a3865f21f666650264890af90e925
Tabish1511 <khaqantabish@gmail.com>
2024-07-18 22:15:38 +0200

    nextauth - FIX: correct redirects

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,16 +2,20 @@
 
 import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Appbar from "@repo/ui/appBar";
 
 export default function Home() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   useEffect(() => {
-    if (!session && status !== "loading") {
-      signIn();
+    if (status === "loading") return; // Do nothing while loading
+    if (session) {
+      router.push('/chat');
     }
-  }, [session, status]);
+  }, [session, status, router]);
+  
 
   if(status === "loading"){
     return <div>Loading...</div>
