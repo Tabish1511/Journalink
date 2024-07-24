aac6cd70fd9c59334a1cae6297c3fa7c9c70a0f7
Tabish1511 <khaqantabish@gmail.com>
2024-07-24 22:46:05 +0200

    db - adding messages to docker db, deploy

--- apps/api/src/websocket.ts
+++ apps/api/src/websocket.ts
@@ -7,7 +7,21 @@ const prisma = new PrismaClient();
 const wss = new WebSocketServer({ server: httpServer, path: '/api/v1/chatWs' });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data, isBinary) {
+  ws.on('message', async function message(data, isBinary) {
+
+    try {
+      await prisma.message.create({
+        data: {
+          content: data.toString(),
+        },
+      });
+      console.log('Message saved to database');
+    } catch (error) {
+      console.error('Error saving message to database:', error);
+    }
+
+
+
     wss.clients.forEach(function each(client) {
       if (client.readyState === 1) {
         client.send(data, { binary: isBinary });
