81caf5ae9ded49f0911afb323c84837f3b0a777b
Tabish1511 <khaqantabish@gmail.com>
2024-07-29 21:41:58 +0200

    chat - overflow scroll feature

--- packages/ui/src/allMessagesComponent.tsx
+++ packages/ui/src/allMessagesComponent.tsx
@@ -8,8 +8,8 @@ interface allMessagesProps {
 export function AllMessagesComponent(props: allMessagesProps) {
     const messagesArray = Object.entries(props.messagesObject);
     
-    return <div className="bg-gray-800 rounded-lg w-full h-lvh my-20 flex flex-col justify-center">
-            {Object.entries(props.messagesObject).map(([key, value]) => (
+    return <div className="bg-gray-800 rounded-lg w-full h-svh overflow-auto my-20 flex flex-col-reverse justify-start">
+            {Object.entries(props.messagesObject).reverse().map(([key, value]) => (
             <div key={key}>
                 <MessageComponent message={value} /> <br/>
             </div>
