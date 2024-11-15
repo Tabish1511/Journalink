53794691f5b263170808847506989cff5cbe831e
Tabish1511 <khaqantabish@gmail.com>
2024-11-15 14:55:38 +0200

    created a local env without bgworker spin up in chatcomp

--- packages/ui/src/chatComponent.tsx
+++ packages/ui/src/chatComponent.tsx
@@ -1,72 +1,80 @@
-"use client"
-import { useEffect, useState, useRef } from 'react';
-import axios from 'axios'; // Import axios
-import { AllMessagesComponent } from '@repo/ui/allMessagesComponent';
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { AllMessagesComponent } from "@repo/ui/allMessagesComponent";
 
 export default function ChatComponent() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const [allMessages, setAllMessages] = useState<string[]>([]);
   const [message, setMessage] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(true); // State to track loading of messages
+  const [isLoading, setIsLoading] = useState(true);
   const baseEndpoint = process.env.NEXT_PUBLIC_API_URL as string;
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-
-    // Ensure the environment variable for Redis worker URL is defined
     if (!process.env.NEXT_PUBLIC_REDIS_WORKER_URL) {
       console.error("REDIS_WORKER_URL is not defined.");
-      setIsLoading(true); 
+      setIsLoading(true);
       return;
     }
 
-    const getAllMessagesPromise = axios.get(`${baseEndpoint}/api/v1/message/messages`);
-    const spinUpRedisWorkerPromise = axios.get(process.env.NEXT_PUBLIC_REDIS_WORKER_URL as string);
+    const getAllMessagesPromise = axios.get(
+      `${baseEndpoint}/api/v1/message/messages`
+    );
 
-    Promise.all([getAllMessagesPromise, spinUpRedisWorkerPromise])  
-      .then(([messagesResponse, redisWorkerResponse]) => {
-        // console.log('Messages fetched:', messagesResponse);
-        const prevMessages = messagesResponse.data.map((message: any) => message.content);
+    Promise.all([getAllMessagesPromise])
+      .then(([messagesResponse]) => {
+        const prevMessages = messagesResponse.data.map(
+          (message: any) => message.content
+        );
         setAllMessages(prevMessages);
-        setIsLoading(false); // Set loading to false after BOTH promises are resolved
+        setIsLoading(false);
       })
-      .catch(error => console.error("Failed to fetch messages:", error));
+      .catch((error) => console.error("Failed to fetch messages:", error));
 
     const newSocket = new WebSocket(`${baseEndpoint}/api/v1/chatWs`);
     newSocket.onopen = () => {
-      console.log('Connection established');
+      console.log("Connection established");
       setSocket(newSocket);
     };
+
     newSocket.onmessage = (message) => {
-      console.log('Message received:', message);
-      setAllMessages(prevMessages => [...prevMessages, message.data]);
+      console.log("Message received:", message);
+      setAllMessages((prevMessages) => [...prevMessages, message.data]);
     };
-    
   }, [baseEndpoint]);
 
-  if (!socket || isLoading) { // Check both socket connection and loading state
+  if (!socket || isLoading) {
     return <div>Loading messages...</div>;
   }
 
   return (
-    <div className='h-screen flex justify-center'>
-      <div className='w-2/6 flex flex-col justify-center'>
+    <div className="h-screen flex justify-center">
+      <div className="w-2/6 flex flex-col justify-center">
         <AllMessagesComponent messagesObject={allMessages} />
-        <div className='flex justify-center'>
-          <input ref={inputRef} autoFocus className='rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4'
+        <div className="flex justify-center">
+          <input
+            ref={inputRef}
+            autoFocus
+            className="rounded-lg bg-indigo-800 border-sky-500 w-4/5 py-4"
             onChange={(e) => setMessage(e.target.value)}
             value={message}
-            placeholder='Type your message'></input>
-          
-          <button className='rounded-lg bg-indigo-800 border-sky-500 w-32 py-4'
+            placeholder="Type your message"
+          />
+          <button
+            className="rounded-lg bg-indigo-800 border-sky-500 w-32 py-4"
             onClick={() => {
-              if (message.trim() === '') return; // Prevent sending if message is empty or only whitespace
-              socket?.send(message); // Optional chaining in case socket is null
-              setMessage(''); // Clear the input after sending the message
-              inputRef.current?.focus(); // Set focus back to the input field
-            }}>Send</button>
+              if (message.trim() === "") return;
+              socket?.send(message);
+              setMessage("");
+              inputRef.current?.focus();
+            }}
+          >
+            Send
+          </button>
         </div>
-      </div>   
+      </div>
     </div>
   );
 }
