dc0d429e3c7d939c5342d560bd4f947f3042fbc0
Tabish1511 <khaqantabish@gmail.com>
2024-12-17 20:37:41 +0000

    4th - (CORS issue in the backend sort out) only /signup with cookie set for session as well as auth, /signin & /logout pending

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -27,6 +27,14 @@ app.use(cors({
   credentials: true,
 }));
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); // No content for OPTIONS requests
+});
+
 
 app.use(express.json());
 
